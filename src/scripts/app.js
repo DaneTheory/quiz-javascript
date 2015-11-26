6a0451e8bc8e11db617bfc7c23f80e4c7c3e92d8
Branden Dane <branden@danetheory.com>
2015-11-26 06:17:03 -0500

    Decipher message via Key
    Created global Object Literal. This gets referenced within TheQuestion.
    Once TheQuestion.gottaCatchEmAll is invoked, the hidden message is
    logged to the console in a human readable format.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,27 +1,39 @@
-(function() {
+var cypherKey = {
+      // Object that holds:
+      // 1.) Message to scramble/unscramble
+      // 2.) Times to scramble/unscramble
+      key: 'span[hidden]',
+      publicAccess: function() {
+        return this.key;
+      }
+}
 
-	var TheQuestion = {
+var TheQuestion = (function() {
+  this.hiddenMessage = cypherKey.key;
+  var selector = this.hiddenMessage;
 
-		gottaCatchEmAll: function(selector) {
-        // var nodes = document.querySelectorAll(selector);
-        // console.log(({}).toString.call(nodes).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
-        // RETURNS nodelist
-	      var convertArr = Array.prototype.slice.call(document.querySelectorAll(selector));
-        // console.log(({}).toString.call(convertArr).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
-        // RETURNS array
-        //
-        var sentence = [];
-        convertArr.forEach(function(el) {
-          var letters = el.textContent;
-          sentence.push(letters);
-        })
-        var hiddenMessage = sentence.join("");
-        // console.log(({}).toString.call(hiddenMessage).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
-        // RETURNS string
-        //
-        return hiddenMessage;
-		}
-  }
-	TheQuestion.gottaCatchEmAll('span[hidden]');
-  console.log(TheQuestion.gottaCatchEmAll('span[hidden]'));
-})();
+  return {
+  		gottaCatchEmAll: function() {
+          // var nodes = document.querySelectorAll(selector);
+          // console.log(({}).toString.call(nodes).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
+          // RETURNS nodelist
+  	      var convertArr = Array.prototype.slice.call(document.querySelectorAll(selector));
+          // console.log(({}).toString.call(convertArr).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
+          // RETURNS array
+          //
+          var sentence = [];
+          convertArr.forEach(function(el) {
+            var letters = el.textContent;
+            sentence.push(letters);
+          })
+          var hiddenMessage = sentence.join("");
+          // console.log(({}).toString.call(hiddenMessage).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
+          // RETURNS string
+          //
+          console.log(hiddenMessage);
+  		}
+  };
+
+}(cypherKey));
+
+TheQuestion.gottaCatchEmAll();
