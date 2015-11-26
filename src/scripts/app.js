7f2def1b1bdcbed5bf366e1151ef0a1476dc048d
Branden Dane <branden@danetheory.com>
2015-11-26 04:17:52 -0500

    merge fix finalized
    Resolved merge fix. Coffee. Thank you.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,6 @@
 
 	var TheQuestion = {
 
-<<<<<<< HEAD
 		gottaCatchEmAll: function(selector) {
         // var nodes = document.querySelectorAll(selector);
         // console.log(({}).toString.call(nodes).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
@@ -25,32 +24,4 @@
   }
 	TheQuestion.gottaCatchEmAll('span[hidden]');
   console.log(TheQuestion.gottaCatchEmAll('span[hidden]'));
-=======
-    // Converts NodeList to native Array.prototype method friendly Array.
-		gottaCatchEmAll: function(selector) {
-	      var convertArr = Array.prototype.slice.call(document.querySelectorAll(selector));
-        return convertArr;
-		},
-
-    // Maps through length of converted array, gets inner letters from HTML,
-    // then pushs these letters into thier own array.
-		theLetters: function() {
-        this.gottaCatchEmAll().map(function(els, i){
-          var len = els.length;
-          var txtArr = [];
-          var letters = els.textContent.split('').join("").substr(0, 1);
-          txtArr.push(letters);
-            return txtArr;
-        })
-		},
-
-    // Combines all arrays into one single array, then converts that
-    // to a single, easily readable string.
-    theSentence: function() {
-        this.theLetters();
-    }
-  }
-	TheQuestion.gottaCatchEmAll('span[hidden]');
-  
->>>>>>> master
 })();
