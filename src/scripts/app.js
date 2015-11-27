09eff2bec60e146b99d122b1de1451881400b839
Branden Dane <branden@danetheory.com>
2015-11-27 16:20:46 -0500

    Playing with styling ideas
    Committing just to save this work as reference. Added in styling and
    wrote JS for animating. All of it is horribly performance heavy. This
    was more for fun and brain storming than anything.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,39 +1,91 @@
 var cypherKey = {
-      // Object that holds:
-      // 1.) Message to scramble/unscramble
-      // 2.) Times to scramble/unscramble
-      key: 'span[hidden]',
-      publicAccess: function() {
-        return this.key;
-      }
-}
+      key: 'span[hidden]'
+};
 
 var TheQuestion = (function() {
-  this.hiddenMessage = cypherKey.key;
-  var selector = this.hiddenMessage;
+  var selector = cypherKey.key;
+  var sentence = [];
+  var message;
+  var convertArr;
+  // var parser = new DOMParser();
 
   return {
+      buildMatrix: function() {
+
+        var eQuote = document.querySelector("#neat");
+
+        // var regex = /\ /;
+        // save the original paragraph as array of words
+        // regex = /[,.?!;:]/; /* Uncomment for sentences */
+        // var aQuote = eQuote.innerHTML.split(regex);
+
+        // wrap each word with a span
+        // eQuote.innerHTML = "";
+        // for (var word in aQuote) {
+        //   eQuote.innerHTML += "<span>" + aQuote[word] + "</span>";
+        // }
+        // ...and save them for later
+        var eWords = document.querySelectorAll("span");
+
+        var repeat = setInterval(function() {
+          if (Math.random() > 0.99) fClearAllHighlights(eQuote);
+          fHighlightRandomWord(eWords);
+        }, 10);
+
+        function fHighlightRandomWord(e) {
+          var iRandom = Math.floor(Math.random() * e.length);
+          e[iRandom].classList.add("highlight");
+        }
+
+        function fClearAllHighlights(e) {
+          var nlHighlights = e.querySelectorAll(".highlight");
+          // convert the nodeList into an array
+          var aHighlights = Array.prototype.slice.call(nlHighlights);
+          // remove .highlight from the spans which have it
+          Array.prototype.map.call(aHighlights, function() {
+            arguments[0].classList.remove("highlight");
+          });
+        }
+
+
+      },
+
   		gottaCatchEmAll: function() {
-          // var nodes = document.querySelectorAll(selector);
-          // console.log(({}).toString.call(nodes).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
-          // RETURNS nodelist
-  	      var convertArr = Array.prototype.slice.call(document.querySelectorAll(selector));
-          // console.log(({}).toString.call(convertArr).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
-          // RETURNS array
-          //
-          var sentence = [];
+          this.buildMatrix();
+          convertArr = Array.prototype.slice.call(document.querySelectorAll(selector));
           convertArr.forEach(function(el) {
-            var letters = el.textContent;
+             var letters = el.textContent;
             sentence.push(letters);
           })
-          var hiddenMessage = sentence.join("");
-          // console.log(({}).toString.call(hiddenMessage).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
-          // RETURNS string
-          //
-          console.log(hiddenMessage);
-  		}
+          message = sentence.join("");
+          console.log('HIDDEN MESSAGE: ' + message);
+  		},
+
+      buildInterface: function() {
+
+
+
+
+        // var hiddenMessageEl = '<div id="inputDecipher">';
+        //     hiddenMessageEl += '<input type="text" ';
+        //     hiddenMessageEl += 'value="';
+        //     hiddenMessageEl += message;
+        //     hiddenMessageEl +='"/>';
+        //     hiddenMessageEl += '<div class="border-line"></div>';
+        //     hiddenMessageEl += '</div>';
+
+        // var parsedEl = parser.parseFromString(hiddenMessageEl, "text/xml");
+        // var newDOMEl = parsedEl.firstChild;
+        // document.body.insertBefore(newDOMEl, document.body.firstChild);
+      },
+
+      init: function() {
+        this.gottaCatchEmAll();
+        // this.buildInterface();
+      }
   };
 
 }(cypherKey));
 
-TheQuestion.gottaCatchEmAll();
+// TheQuestion.gottaCatchEmAll();
+TheQuestion.init();
